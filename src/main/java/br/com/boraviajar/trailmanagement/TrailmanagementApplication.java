d596fe5427c0d7ad1975639a98b705eda6fc8b2d
Lenilson Teixeira <lenilsonts@gmail.com>
2020-02-15 22:33:43 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/boraviajar/trailmanagement/TrailmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.boraviajar.trailmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/boraviajar/trailmanagement/TrailmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boraviajar.trailmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
